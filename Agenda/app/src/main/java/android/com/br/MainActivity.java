04161bc1742b2402c9c3db22d836d9d5d52c62f9
gilcemar <gilcemargz@hotmail.com>
2022-03-13 18:42:51 -0300

    first

---
+++ Agenda/app/src/main/java/android/com/br/MainActivity.java
@@ -0,0 +1,15 @@
+package android.com.br;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Olá mundo", Toast.LENGTH_LONG).show();
+    }
+}
