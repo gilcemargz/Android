c5da551d66dc8e27d67e410e458da223b8de339d
gilcemar <gilcemargz@hotmail.com>
2022-03-13 21:09:22 -0300

    layout

--- Agenda/app/src/main/java/android/com/br/MainActivity.java
+++ Agenda/app/src/main/java/android/com/br/MainActivity.java
@@ -2,6 +2,7 @@ package android.com.br;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -10,6 +11,9 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Olá mundo", Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "Olá mundo", Toast.LENGTH_LONG).show();
+        //TextView aluno = new TextView(this);
+        //aluno.setText("Oĺa Gilcemar");
+        setContentView(R.layout.activity_main);
     }
 }
