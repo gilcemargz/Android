f4a61edc5ce8a4e5b5a6e2c71e15325a3af15c31
gilcemar <gilcemargz@hotmail.com>
2022-03-15 21:23:49 -0300

    layout

--- Agenda/app/src/main/java/android/com/br/MainActivity.java
+++ Agenda/app/src/main/java/android/com/br/MainActivity.java
@@ -2,10 +2,17 @@ package android.com.br;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
     @Override
@@ -15,5 +22,21 @@ public class MainActivity extends Activity {
         //TextView aluno = new TextView(this);
         //aluno.setText("Oĺa Gilcemar");
         setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<String>(Arrays.asList("Gil", "Jackson","Julia"));
+
+        ListView viewAlunos = findViewById(R.id.acitivity_main_lista_alunos);
+
+        viewAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos ));
+
+
+        /*
+        TextView al1 = findViewById(R.id.textView);
+        TextView al2 = findViewById(R.id.textView2);
+        TextView al3 = findViewById(R.id.textView3);
+
+        al1.setText(alunos.get(0));
+        al2.setText(alunos.get(1));
+        al3.setText(alunos.get(2));*/
+
     }
 }
